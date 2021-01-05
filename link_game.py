d16377aecd644ff6c1613daf89f7280156c0c8be
Wan Wei <goodnightww@gmail.com>
2021-01-04 22:59:46 -0800

    fine cut the blocks

--- link_game.py
+++ link_game.py
@@ -5,7 +5,7 @@ from PIL import ImageGrab, Image
 class LinkGameRobot:
     def __init__(self):
         self.window_name = "QQ游戏 - 连连看角色版"
-        self.top_left_and_bot_right = v = (893, 581, 1483, 964)
+        self.top_left_and_bot_right = v = (894, 582, 1482, 965)
         self.row_block_num = 19
         self.col_block_num = 11
         self.block_h = (v[3] - v[1]) / self.col_block_num  # 34.8
@@ -26,6 +26,21 @@ class LinkGameRobot:
         """
         image = ImageGrab.grab(self.top_left_and_bot_right)
         image.show()
+
+        img_blocks = \
+            [[None] * self.col_block_num for i in range(self.row_block_num)]
+        for x in range(1, self.row_block_num):
+            for y in range(self.col_block_num):
+                top_left_x = x * self.block_w
+                top_left_y = y * self.block_h
+                bot_right_x = (x + 1) * self.block_w
+                bot_right_y = (y + 1) * self.block_h
+
+                box = (top_left_x, top_left_y, bot_right_x, bot_right_y)
+                img_block = image.crop(box)
+                img_block.show()
+            input()
+
 
 
 if __name__ == "__main__":
